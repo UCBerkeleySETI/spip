a52c79a289bc3068d092fe6d5c2634b60f9808c6
Andrew Jameson <andrew.jameson@gmail.com>
2018-03-12 02:10:17 +0200

    wrapped socket commns in try/except

--- scripts/spip/threads/reporting_thread.py
+++ scripts/spip/threads/reporting_thread.py
@@ -9,7 +9,7 @@ from socketed_thread import SocketedThread
 
 from xmltodict import parse
 from xml.parsers.expat import ExpatError
-import socket
+import socket, errno
 
 class ReportingThread (SocketedThread):
 
@@ -21,42 +21,59 @@ class ReportingThread (SocketedThread):
 
   def process_message_on_handle (self, handle):
 
-    raw = handle.recv(4096)
-    message = raw.strip()
-    self.script.log (2, "ReportingThread: message="+str(message))
+    # try to read from the socket
+    try:
+      raw = handle.recv(4096)
+    except socket.error, e:
+      if e.errno == errno.ECONNRESET:
+        self.script.log(2, "ReportingThread::process_message_on_handle closing connection")
+        handle.close()
+        self.script.log(2, "ReportingThread::process_message_on_handle removing socket from can_read")
+        for i, x in enumerate(self.can_read):
+          if (x == handle):
+            del self.can_read[i]
 
-    if len(message) == 0:
-      handle.close()
-      for i, x in enumerate(self.can_read):
-        if (x == handle):
-          del self.can_read[i]
+      else:
+        raise e
 
+    # the socket read was successful
     else:
-      try:
-       xml = parse(message)
-      except ExpatError as e:
-        handle.send ("<xml>Malformed XML message</xml>\r\n")
+
+      message = raw.strip()
+      self.script.log (2, "ReportingThread::process_message_on_handle message="+str(message))
+
+      if len(message) == 0:
         handle.close()
         for i, x in enumerate(self.can_read):
           if (x == handle):
             del self.can_read[i]
 
-      self.script.log(3, "<- " + str(xml))
-
-      retain, reply = self.parse_message (xml)
-
-      if retain:
-        bytes_sent = handle.send (reply)
-        self.script.log (2, "ReportingThread: sent " + str(bytes_sent) + " bytes")
       else:
-        bytes_sent = handle.sendall (reply)
-        self.script.log (2, "ReportingThread: sent " + str(bytes_sent) + " bytes")
+        try:
+          xml = parse(message)
+        except ExpatError as e:
+          handle.send ("<xml>Malformed XML message</xml>\r\n")
+          handle.close()
+          for i, x in enumerate(self.can_read):
+            if (x == handle):
+              del self.can_read[i]
 
-        self.script.log (3, "ReportingThread: handle.shutdown()")
-        handle.shutdown(socket.SHUT_RDWR)
-        self.script.log (3, "ReportingThread: handle.close()")
-        handle.close()
-        for i, x in enumerate(self.can_read):
-          if (x == handle):
-                del self.can_read[i]
+        self.script.log(3, "<- " + str(xml))
+
+        retain, reply = self.parse_message (xml)
+
+        if retain:
+          bytes_sent = handle.send (reply)
+          self.script.log (2, "ReportingThread: sent " + str(bytes_sent) + " bytes")
+        else:
+          bytes_sent = handle.sendall (reply)
+          self.script.log (2, "ReportingThread: sent " + str(bytes_sent) + " bytes")
+
+          self.script.log (3, "ReportingThread: handle.shutdown()")
+          handle.shutdown(socket.SHUT_RDWR)
+          self.script.log (3, "ReportingThread: handle.close()")
+          handle.close()
+          for i, x in enumerate(self.can_read):
+            if (x == handle):
+                  del self.can_read[i]
 
