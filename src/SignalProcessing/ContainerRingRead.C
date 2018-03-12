86ceb8f5b8bd114002ae3aab5aff09e88f9c4eaa
Andrew Jameson <andrew.jameson@gmail.com>
2018-03-12 20:06:46 +1100

    Use the parent class read_header

--- src/SignalProcessing/ContainerRingRead.C
+++ src/SignalProcessing/ContainerRingRead.C
@@ -32,31 +32,8 @@ void spip::ContainerRingRead::process_header ()
   // load this header from the c string
   header.load_from_str (header_str);
 
-  if (header.get ("NANT", "%u", &nsignal) != 1)
-    throw invalid_argument ("NANT did not exist in header");
-
-  if (header.get ("NCHAN", "%u", &nchan) != 1)
-    throw invalid_argument ("NCHAN did not exist in header");
-
-  if (header.get ("NBIT", "%u", &nbit) != 1)
-    throw invalid_argument ("NBIT did not exist in header");
-
-  if (header.get ("NPOL", "%u", &npol) != 1)
-    throw invalid_argument ("NPOL did not exist in header");
-
-  if (header.get ("NDIM", "%u", &ndim) != 1)
-    throw invalid_argument ("NDIM did not exist in header");
-
-  char order[5];
-  if (header.get ("ORDER", "%s", order) != 1)
-    throw invalid_argument ("ORDER did not exist in header");
-  if (strcmp (order, "TSPF") == 0)
-    set_order (spip::Ordering::TSPF);
-  else if (strcmp (order, "SFPT") == 0)
-    set_order (spip::Ordering::SFPT);
-  else
-    throw invalid_argument ("unsupported input order");
-
+  // read the mandatory header parameters
+  spip::Container::read_header();
 
   nbits_per_sample = nsignal * nchan * nbit * npol * ndim;
   ndat = size / (nbits_per_sample / 8);
