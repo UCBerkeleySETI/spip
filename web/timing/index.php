5608cea9a0db74447d29d96990f0999db85a3ceb
Andrew Jameson <andrew.jameson@gmail.com>
2018-03-12 06:05:44 +0200

    changed port definition to BEAM_ from STREAM_

--- web/timing/index.php
+++ web/timing/index.php
@@ -274,12 +274,12 @@ class timing extends spip_webpage
       if ($this->config["INDEPENDENT_BEAMS"] == "true")
       {
         $host = $beam["host"];
-        $port = $this->config["STREAM_REPACK_PORT"] + $ibeam;
+        $port = $this->config["BEAM_REPACK_PORT"] + $ibeam;
       }
       else
       {
         $host = $this->config["SERVER_HOST"];
-        $port = $this->config["STREAM_REPACK_PORT"] + $ibeam;
+        $port = $this->config["BEAM_REPACK_PORT"] + $ibeam;
       }
 
       if ($repack_socket->open ($host, $port, 0) == 0)
@@ -369,7 +369,7 @@ class timing extends spip_webpage
       {
         $host = $this->config["SERVER_HOST"];
       }
-      $port = $this->config["STREAM_REPACK_PORT"];
+      $port = $this->config["BEAM_REPACK_PORT"];
 
       if ($ibeam >= 0)
         $port += $ibeam;
